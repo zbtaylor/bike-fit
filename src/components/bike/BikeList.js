d9ff9a7afd1b6c58764e993f3f05b916f6f06450
Zach Taylor <zbtaylor1@gmail.com>
2019-08-27 14:24:08 -0400

    created bike list with links to view and edit individual bikes

--- src/components/bike/BikeList.js
+++ src/components/bike/BikeList.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+import { List, Button } from "semantic-ui-react";
 
-const BikeList = () => {
+const BikeList = props => {
+  const { bikes } = props;
+
   return (
-    <h2>Bike List</h2>
-  )
-}
+    <List divided verticalAlign="middle" className="bikeList">
+      {bikes.map(bike => {
+        return (
+          <List.Item>
+            <List.Content floated="right">
+              <Button compact basic href={`/bikes/${bike.id}`}>
+                View
+              </Button>
+              <Button compact href={`/bikes/${bike.id}/edit`}>
+                Edit
+              </Button>
+            </List.Content>
+            <List.Content>
+              <List.Header>{bike.name}</List.Header>
+              {bike.brand} {bike.model}
+            </List.Content>
+          </List.Item>
+        );
+      })}
+    </List>
+  );
+};
 
 export default BikeList;
--- src/components/bike/BikesPage.js
+++ src/components/bike/BikesPage.js
@@ -2,19 +2,51 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Button } from "semantic-ui-react";
 import MainNav from "../nav/MainNav";
+import BikesList from "./BikeList";
+
+const bikes = [
+  {
+    id: 0,
+    name: "Rocket",
+    brand: "Specialized",
+    model: "Allez Sprint",
+    weight: "19 lbs",
+    type: "Road"
+  },
+  {
+    id: 1,
+    name: "Blackbird",
+    brand: "Cannondale",
+    model: "SuperSix Evo",
+    weight: "17 lbs",
+    type: "Road"
+  },
+  {
+    id: 2,
+    name: "Abominable",
+    brand: "Yeti",
+    model: "SB150",
+    weight: "22 lbs",
+    type: "Mountain"
+  }
+];
 
 const BikesPage = props => {
   return (
     <Container>
       <MainNav />
-      <h2>Bikes</h2>
-      <Button
-        as={Link}
-        to="/bikes/add"
-        floated="right"
-        content="Add Bike"
-        primary
-      />
+      <Container>
+        <Button
+          as={Link}
+          to="/bikes/add"
+          floated="right"
+          content="Add Bike"
+          className="addBikeBtn"
+          primary
+        />
+        <h2>Bikes</h2>
+      </Container>
+      <BikesList bikes={bikes} />
     </Container>
   );
 };
