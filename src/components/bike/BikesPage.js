ab221667a6c881b555dfdda76a9efc610333b593
Zach Taylor <zbtaylor1@gmail.com>
2019-08-27 16:59:08 -0400

    added grid to bikepage to match spacing with bikedetail

--- src/components/bike/BikesPage.js
+++ src/components/bike/BikesPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Button } from "semantic-ui-react";
+import { Container, Button, Grid } from "semantic-ui-react";
 import MainNav from "../nav/MainNav";
 import BikesList from "./BikesList";
 
@@ -10,15 +10,19 @@ const BikesPage = props => {
   return (
     <Container>
       <MainNav />
-      <h2>Bikes</h2>
-      <BikesList bikes={bikes} />
-      <Button
-        as={Link}
-        to="/bikes/add"
-        content="Add Bike"
-        className="addBikeBtn"
-        primary
-      />
+      <Grid columns={1}>
+        <Grid.Column>
+          <h2>Bikes</h2>
+          <BikesList bikes={bikes} />
+          <Button
+            as={Link}
+            to="/bikes/add"
+            content="Add Bike"
+            className="addBikeBtn"
+            primary
+          />
+        </Grid.Column>
+      </Grid>
     </Container>
   );
 };
--- src/components/nav/MainNav.js
+++ src/components/nav/MainNav.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 const MainNav = () => {
-
   return (
     <Menu>
-      <Menu.Item as={NavLink} name='home' to='/' exact />
-      <Menu.Item as={NavLink} name='bikes' to='/bikes' />
+      <Menu.Item as={NavLink} name="home" to="/" exact />
+      <Menu.Item as={NavLink} name="bikes" to="/bikes" />
     </Menu>
-  )
-}
+  );
+};
 
 export default MainNav;
