3e84bf029883a2273b83c839cb9c03d148f6bad6
Zach Taylor <zbtaylor1@gmail.com>
2019-11-11 12:58:27 -0500

    bike view component now looking at server for data

--- client/src/components/bike/BikeView.js
+++ client/src/components/bike/BikeView.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BikeForm from "./BikeForm";
-import { bikes } from "../../Server";
+import Axios from "axios";
 
 const BikeDetail = props => {
   const id = props.match.params.id;
-  const bike = bikes[id];
+  const [bike, setBike] = useState({});
+
+  useEffect(() => {
+    Axios.get(`http://localhost:9000/bikes/${id}`)
+      .then(res => {
+        setBike(res.data);
+      })
+      .catch(err => {
+        // handle error
+      });
+  }, []);
 
   return <BikeForm bike={bike} id={id} disabled={true} {...props} />;
 };
