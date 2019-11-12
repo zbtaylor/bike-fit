bcae69a42f53797ed9f73280529ae3f95a8dca0d
Zach Taylor <zbtaylor1@gmail.com>
2019-11-12 18:19:07 -0500

    created user migration and seed files

--- data/migrations/20191112180648_user.js
+++ data/migrations/20191112180648_user.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl
+      .string("email", 256)
+      .unique()
+      .notNullable();
+    tbl.string("password", 128).notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("users");
 };
