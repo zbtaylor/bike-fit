628d9e26e433e3533506dcd28891cfb6dbc589cc
Zach Taylor <zbtaylor1@gmail.com>
2019-11-12 17:23:03 -0500

    moved bike db helper methods

--- routes/bikes.js
+++ routes/bikes.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const Bikes = require("../data/Bikes.js");
+const Bikes = require("../data/models/bike-model.js");
 
 // GET
 router.get("/", (req, res) => {
