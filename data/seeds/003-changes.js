936ddd4d3a1679c73b4996062356fbc6f253d0f2
Zach Taylor <zbtaylor1@gmail.com>
2019-11-27 14:46:06 -0500

    updated changes seed to use postgres date format

--- data/seeds/003-changes.js
+++ data/seeds/003-changes.js
@@ -9,32 +9,32 @@ exports.seed = function(knex) {
           id: 1,
           description: "Changed to an 80mm stem from 100mm.",
           notes: "Felt great.",
-          created_on: "02/03/2019",
-          last_edited: "02/26/2019",
+          created_on: "2019-02-03",
+          last_edited: "2019-02-26",
           bike_id: 1
         },
         {
           id: 2,
           description: "Moved saddle forward 2mm.",
           notes: "Felt more pain in wrists.",
-          created_on: "06/10/2019",
-          last_edited: "07/18/2019",
+          created_on: "2019-06-10",
+          last_edited: "2019-07-18",
           bike_id: 1
         },
         {
           id: 3,
           description: "Added 5mm spacer to steerer tube.",
           notes: "Felt less back pain.",
-          created_on: "01/16/2019",
-          last_edited: "01/20/2019",
+          created_on: "2019-01-16",
+          last_edited: "2019-01-20",
           bike_id: 2
         },
         {
           id: 4,
           description: "Swapped to a new saddle.",
           notes: "Felt more connection with sit bones.",
-          created_on: "10/22/2019",
-          last_edited: "11/07/2019",
+          created_on: "2019-10-22",
+          last_edited: "2019-11-07",
           bike_id: 3
         }
       ]);
