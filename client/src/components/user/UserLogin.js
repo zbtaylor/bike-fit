d0e793fab5fa50203138c8e73698e95df1df2dd0
Zach Taylor <zbtaylor1@gmail.com>
2019-11-27 13:53:36 -0500

    added links between login and register forms

--- client/src/components/user/UserLogin.js
+++ client/src/components/user/UserLogin.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import UserForm from "./UserForm";
 
 const UserLogin = props => {
@@ -6,6 +7,7 @@ const UserLogin = props => {
     <>
       <h2>Login</h2>
       <UserForm {...props} />
+      <Link to="/register">Not a user?</Link>
     </>
   );
 };
