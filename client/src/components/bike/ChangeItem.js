7a3acf6636302e3eae25edd46aa1617336cb083b
Zach Taylor <zbtaylor1@gmail.com>
2019-11-27 20:46:48 -0500

    removed manual date entry, using database timestamp column instead

--- client/src/components/bike/ChangeItem.js
+++ client/src/components/bike/ChangeItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Comment } from "semantic-ui-react";
 
-const ChangeItem = ({ description, lastEdited }) => {
+const ChangeItem = ({ description, created }) => {
   return (
     <Comment>
       <Comment.Content>
-        <Comment.Metadata>{lastEdited}</Comment.Metadata>
+        <Comment.Metadata>{created}</Comment.Metadata>
         <Comment.Text>{description}</Comment.Text>
       </Comment.Content>
     </Comment>
