59ad33b5862e1d6c91a1b8e0005a447278d963f4
Zach Taylor <zbtaylor1@gmail.com>
2020-01-16 11:13:22 -0500

    updated bike create page to match bike view

--- client/src/components/bike/BikeCreate.js
+++ client/src/components/bike/BikeCreate.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Menu, Segment, Grid, Sticky } from "semantic-ui-react";
 import BikeForm from "./BikeForm";
+import ChangeList from "./ChangeList";
 import MainNav from "../nav/MainNav";
+import Axios from "axios";
+import { SizeMe } from "react-sizeme";
+import MeasurementVisualizer from "../measurement/MeasurementVisualizer";
+import MeasurementDescription from "../measurement/MeasurementDescription";
 
 const BikeCreate = props => {
+  const [hovered, setHovered] = useState(null);
+
   const newBike = {
     id: 0,
     nickname: "",
@@ -17,7 +25,26 @@ const BikeCreate = props => {
   return (
     <>
       <MainNav {...props} />
-      <BikeForm bike={newBike} newBike={true} {...props} />
+      <Grid columns={2}>
+        <SizeMe
+          render={({ size }) => (
+            <Grid.Column>
+              <Sticky offset={30}>
+                <MeasurementVisualizer width={size.width} hovered={hovered} />
+                <MeasurementDescription hovered={hovered} />
+              </Sticky>
+            </Grid.Column>
+          )}
+        />
+        <Grid.Column>
+          <BikeForm
+            bike={newBike}
+            disabled={false}
+            setHovered={setHovered}
+            {...props}
+          />
+        </Grid.Column>
+      </Grid>
     </>
   );
 };
