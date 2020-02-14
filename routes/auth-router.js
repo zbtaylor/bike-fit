06fd576e8bcc9bb9b8f1d9c5d1d5b7e55294f2b1
Zach Taylor <zbtaylor1@gmail.com>
2020-02-13 21:08:02 -0500

    sending basic transactional emails from sendinblue

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -3,18 +3,46 @@ var router = express.Router();
 const bcrypt = require("bcryptjs");
 const Users = require("../models/user-model.js");
 const { generateToken } = require("../middleware/token.js");
+const nodemailer = require("nodemailer");
 
-router.post("/register", (req, res, next) => {
+const sendMail = async user => {
+  let transporter = nodemailer.createTransport({
+    service: '"SendinBlue"', // no need to set host or port etc.
+    auth: {
+      user: "zbtaylor1@gmail.com",
+      pass: "6JvFMzO0KS2TxgR5"
+    }
+  });
+
+  let info = await transporter.sendMail({
+    from: '"My Bike Fit Journal" <no-reply@mybikefitjournal.com>', // sender address
+    to: "zbtaylor1@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: `${user} signed up`, // plain text body
+    html: `${user} signed up` // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+};
+
+router.post("/register", async (req, res, next) => {
   const newUser = req.body;
   if (newUser.email && newUser.password) {
     const hash = bcrypt.hashSync(newUser.password, 14);
     newUser.password = hash;
     Users.insert(req.body)
       .then(user => {
+        sendMail(user.email);
+      })
+      .then(() => {
         res.status(200).json({ message: "Registration successful" });
       })
       .catch(err => {
-        next(err);
+        res
+          .status(401)
+          .json({ message: `${req.body.email} has already been registered.` });
+        // next(err);
       });
   } else {
     res.status(400).json({ message: "Invalid credentials" });
