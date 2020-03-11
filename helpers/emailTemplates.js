938dbc3a97eb0e33eb31d30498695b09a5e6d455
Zach Taylor <zbtaylor1@gmail.com>
2020-03-11 18:21:06 -0400

    updated email template URLs

--- helpers/emailTemplates.js
+++ helpers/emailTemplates.js
@@ -2,15 +2,15 @@ module.exports = {
   confirmation: hash => {
     return {
       subject: "Email Confirmation - My Bike Fit Journal",
-      text: `Please paste the following link in to your browser's URL bar in order to complete your registration: http://www.mybikefitjournal.com/confirm/${hash}`,
-      html: `Please follow <a href="http://www.mybikefitjournal.com/confirm/${hash}">this link</a> to complete your registration.`
+      text: `Please paste the following link in to your browser's URL bar in order to complete your registration: http://app.mybike.fit/confirm/${hash}`,
+      html: `Please follow <a href="http://app.mybike.fit/confirm/${hash}">this link</a> to complete your registration.`
     };
   },
   reset: hash => {
     return {
       subject: "Password Reset - My Bike Fit Journal",
-      text: `Please paste the following link in to your browser's URL bar in order to reset your password: http://www.mybikefitjournal.com/reset/${hash}`,
-      html: `Please follow <a href="http://www.mybikefitjournal.com/reset/${hash}">this link</a> to reset your password.`
+      text: `Please paste the following link in to your browser's URL bar in order to reset your password: http://app.mybike.fit/reset/${hash}`,
+      html: `Please follow <a href="http://app.mybike.fit/reset/${hash}">this link</a> to reset your password.`
     };
   }
 };
