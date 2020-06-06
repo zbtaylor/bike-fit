bc748cd59bdfaea529bce334e4770de40f0ea0a1
Zach Taylor <zbtaylor1@gmail.com>
2020-06-06 14:37:47 -0400

    removing unnecessary migration

--- data/migrations/20200507231448_redo.js
+++ data/migrations/20200507231448_redo.js
@@ -1,31 +1,7 @@
 exports.up = function (knex) {
-  return knex.schema.table("bikes", (tbl) => {
-    tbl.dropColumn("saddleHeight");
-    tbl.dropColumn("saddleHeightOverBars");
-    tbl.dropColumn("saddleToHandlebar");
-    tbl.dropColumn("saddleAngle");
-    tbl.dropColumn("saddleForeAft");
-    tbl.dropColumn("stemLength");
-    tbl.dropColumn("stemAngle");
-    tbl.dropColumn("handlebarWidth");
-    tbl.dropColumn("handlebarAngle");
-    tbl.dropColumn("brakeLeverPosition");
-    tbl.dropColumn("crankLength");
-  });
+  return true;
 };
 
 exports.down = function (knex) {
-  return knex.schema.table("bikes", (tbl) => {
-    tbl.integer("saddleHeight").unsigned().defaultTo(0);
-    tbl.integer("saddleHeightOverBars").unsigned().defaultTo(0);
-    tbl.integer("saddleToHandlebar").unsigned().defaultTo(0);
-    tbl.decimal("saddleAngle").defaultTo(0);
-    tbl.integer("saddleForeAft").unsigned().defaultTo(0);
-    tbl.integer("stemLength").unsigned().defaultTo(0);
-    tbl.decimal("stemAngle").defaultTo(0);
-    tbl.integer("handlebarWidth").unsigned().defaultTo(0);
-    tbl.decimal("handlebarAngle").defaultTo(0);
-    tbl.integer("brakeLeverPosition").unsigned().defaultTo(0);
-    tbl.decimal("crankLength").unsigned().defaultTo(0);
-  });
+  return true;
 };
