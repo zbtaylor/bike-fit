093677a7e43bbfb9d778bf4078bce4af6280a1e9
Zach Taylor <zbtaylor1@gmail.com>
2021-12-15 14:36:16 -0500

    removed ssl prop from connection string

--- knexfile.js
+++ knexfile.js
@@ -34,7 +34,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    connection: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
     pool: {
       min: 2,
