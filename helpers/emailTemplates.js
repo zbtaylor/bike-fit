2784544c7f18bb391810106977d517b9ce8cc854
Zach Taylor <zbtaylor1@gmail.com>
2021-12-15 14:44:06 -0500

    updated email templates to use the heroku URL

--- helpers/emailTemplates.js
+++ helpers/emailTemplates.js
@@ -1,16 +1,16 @@
 module.exports = {
-  confirmation: hash => {
+  confirmation: (hash) => {
     return {
       subject: "Email Confirmation - My Bike Fit",
-      text: `Please paste the following link in to your browser's URL bar in order to complete your registration: http://app.mybike.fit/confirm/${hash}`,
-      html: `Please follow <a href="http://app.mybike.fit/confirm/${hash}">this link</a> to complete your registration.`
+      text: `Please paste the following link in to your browser's URL bar in order to complete your registration: http://bike-fit.herokuapp.com/confirm/${hash}`,
+      html: `Please follow <a href="http://bike-fit.herokuapp.com/confirm/${hash}">this link</a> to complete your registration.`,
     };
   },
-  reset: hash => {
+  reset: (hash) => {
     return {
       subject: "Password Reset - My Bike Fit",
-      text: `Please paste the following link in to your browser's URL bar in order to reset your password: http://app.mybike.fit/reset/${hash}`,
-      html: `Please follow <a href="http://app.mybike.fit/reset/${hash}">this link</a> to reset your password.`
+      text: `Please paste the following link in to your browser's URL bar in order to reset your password: http://bike-fit.herokuapp.com/reset/${hash}`,
+      html: `Please follow <a href="http://bike-fit.herokuapp.com/reset/${hash}">this link</a> to reset your password.`,
     };
-  }
+  },
 };
