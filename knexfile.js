b49f79174c37f7b7e0ecacc6423e5ab34721d559
Zach Taylor <zbtaylor1@gmail.com>
2021-12-15 14:18:11 -0500

    retrying ssl opt for knex prod config

--- knexfile.js
+++ knexfile.js
@@ -34,10 +34,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     pool: {
       min: 2,
       max: 10,
